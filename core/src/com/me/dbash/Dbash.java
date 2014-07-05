9c500f73c03a4b96c96ceb9414ccb36af9eb3445
stefoid <stevenmathers@yahoo.com>
2014-07-05 20:56:41 +1000

    splash

--- core/src/com/me/dbash/Dbash.java
+++ core/src/com/me/dbash/Dbash.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
@@ -20,6 +21,7 @@ import com.dbash.platform.CachedSpriteManager;
 import com.dbash.platform.FatOutlineFont;
 import com.dbash.platform.SizeCalculator;
 import com.dbash.platform.SmoothBitmapFont;
+import com.dbash.platform.SplashScreen;
 import com.dbash.platform.UIDepend;
 import com.dbash.presenters.root.RootPresenter;
 
@@ -146,3 +148,147 @@ public class Dbash implements ApplicationListener {
 	
 	}
 }
+
+
+
+//public class Dbash implements ApplicationListener {
+//	
+//	Dungeon dungeon;
+//	TurnProcessor turnProcessor;
+//	private SpriteBatch spriteBatch;
+//	CachedSpriteManager spriteManager;
+//	RootPresenter rootPresenter;
+//	Audio audio;
+//	static boolean init = false;
+//    private Texture texture;
+//    private Image splashImage;
+//    private Stage stage;
+//    
+//	public Dbash() {
+//	}
+//
+//	@Override
+//	public void create() {	
+//	    texture = new Texture(Gdx.files.internal("data/splash.png"));
+//	    splashImage = new Image(texture);
+//	    stage = new Stage();
+//		stage.addActor(splashImage);
+//	}
+//	
+//	private void doInit() {
+//		boolean newGame = true;
+//		Gdx.app.setLogLevel(Application.LOG_INFO);
+//		
+//		dungeon = new Dungeon(true);
+//		turnProcessor = new TurnProcessor(dungeon, dungeon, dungeon);
+//		
+//		// Make the dependency buckets to pass to the rootPresenter
+//		spriteManager = new CachedSpriteManager();
+//		spriteBatch = new SpriteBatch();
+//		UIDepend gui = new UIDepend();
+//		
+//		int[] fontSizes = {82, 64, 48, 40, 32, 26, 21, 17};
+//		gui.fonts = new ArrayList<SmoothBitmapFont>();
+//		for (int fontSize : fontSizes) {
+//			gui.fonts.add(new SmoothBitmapFont(fontSize));
+//		}
+//		
+//		gui.spriteManager = spriteManager;
+//		gui.numberFont = new FatOutlineFont();
+//		gui.sizeCalculator = new SizeCalculator();
+//		gui.audio = new Audio();
+//		audio = gui.audio;
+//		
+//		PresenterDepend model = new PresenterDepend();
+//		model.presenterDungeon = dungeon;
+//		model.presenterTurnState = turnProcessor;
+//		
+//		this.rootPresenter = new RootPresenter(gui, model);
+//		Gdx.input.setInputProcessor(rootPresenter);
+//
+//		// load previously saved game, if it exists.
+//		FileHandle fl = Gdx.files.local("gamedata.dat");
+//		if (fl.exists() == true) {
+//			newGame = false;
+//			ObjectInputStream in = null;
+//			try {
+//				in = new ObjectInputStream(fl.read());
+//				
+//				// load the entire list of creatures, so we have them available for future reference via their
+//				// uniqueIds. (minus falling characters which dont appear on the map)
+//				AllCreatures allCreatures = new AllCreatures(in, dungeon, dungeon, turnProcessor);
+//				
+//				// Now tell the turn processor and dungeon to load, and any references to creatures
+//				// they have in their saved files can point into the list of allCreatures.
+//				dungeon.load(in, allCreatures);
+//				turnProcessor.load(in, dungeon, dungeon, dungeon, allCreatures);
+//				gui.audio.load(in);
+//				in.close();
+//				newGame = false;
+//			} catch (Exception e) {
+//				newGame = true;
+//			} 
+//		} 
+//
+//		if (newGame) {
+//			turnProcessor.startNewGame();
+//		} else {
+//			turnProcessor.resume();
+//		}
+//	}
+//	
+//	@Override
+//	public void pause() {
+//		System.out.println("SAVING GAMEDATA");
+//		FileHandle fl = Gdx.files.local("gamedata.dat");
+//		ObjectOutputStream out = null;
+//		try {
+//			out = new ObjectOutputStream(fl.write(false));
+//			turnProcessor.allCreatures.persist(out);
+//			dungeon.persist(out);
+//			turnProcessor.persist(out);
+//			audio.persist(out);
+//		} catch (IOException e) {
+//		} finally {
+//			try {
+//				out.close();
+//			} catch (IOException e) {
+//			}
+//		}
+//	}
+//	
+//	@Override
+//	public void dispose() {
+//		spriteBatch.dispose();
+//		
+//	}
+//
+//	@Override
+//	public void render() {
+//        Gdx.gl.glClearColor(0, 0, 0, 1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        
+//		if (init == false) {
+//	        stage.act();
+//	        stage.draw();
+//			doInit();
+//			init = true;
+//		} else {
+//			// Give the TurnProcessor a tick - it processes one creature or character per call.
+//			turnProcessor.gameLogicLoop();
+//	
+//			// draw everything
+//			rootPresenter.draw(spriteBatch);
+//		}
+//	}
+//
+//	@Override
+//	public void resize(int width, int height) {
+//
+//	}
+//
+//	@Override
+//	public void resume() {
+//	
+//	}
+//}
--- desktop/src/com/me/dbash/desktop/DesktopLauncher.java
+++ desktop/src/com/me/dbash/desktop/DesktopLauncher.java
@@ -7,26 +7,18 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.me.dbash.Dbash;
-//import com.me.dbash.Settings;
-//import com.me.dbash.Main.MaximumApplicationConfiguration;
-//import com.me.dbash.Main.MyApplicationConfiguration;
-//import com.me.dbash.Main.TexturePackerSettings;
 
 public class DesktopLauncher {
-//	public static void main (String[] arg) {
-//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		new LwjglApplication(new Dbash(), config);
-//	}
 	
 	public static class MyApplicationConfiguration extends
 	LwjglApplicationConfiguration {
 		
-	public MyApplicationConfiguration() {
-		super();
-		title = "Dungeon Bash";
-		width = 960;
-		height = 640;
-	}
+		public MyApplicationConfiguration() {
+			super();
+			title = "Dungeon Bash";
+			width = 960;
+			height = 640;
+		}
 	}
 	
 	/**
@@ -59,48 +51,48 @@ public class DesktopLauncher {
 	*
 	*/
 	public static class TexturePackerSettings extends Settings {
-	public TexturePackerSettings() {
-		maxWidth = 1024;
-		maxHeight = 2048;
-		combineSubdirectories = true;
-		flattenPaths = true;
-		filterMin = TextureFilter.Linear;
-		filterMag = TextureFilter.Linear;
-		duplicatePadding = true; // to stop gap line due to oversampling with linear filter
-		useIndexes = true;
-		fast = true;
-	}
+		public TexturePackerSettings() {
+			maxWidth = 1024;
+			maxHeight = 2048;
+			combineSubdirectories = true;
+			flattenPaths = true;
+			filterMin = TextureFilter.Linear;
+			filterMag = TextureFilter.Linear;
+			duplicatePadding = true; // to stop gap line due to oversampling with linear filter
+			useIndexes = true;
+			fast = true;
+		}
 	}
 	
 	public static void main(String[] args) {
-	packTextures();
-	startGame(isFullScreenArgument(args) ? new MaximumApplicationConfiguration()
-			: new MyApplicationConfiguration());
+		packTextures();
+		startGame(isFullScreenArgument(args) ? new MaximumApplicationConfiguration()
+				: new MyApplicationConfiguration());
 	}
 	
 	private static void packTextures() {
-	TexturePacker.process(new TexturePackerSettings(), "pngs",
-			"bin/res/dbash2/pngs-packed", "pack");
+		TexturePacker.process(new TexturePackerSettings(), "pngs",
+				"bin/res/dbash2/pngs-packed", "pack");
 	}
 	
 	private static void startGame(LwjglApplicationConfiguration config) {
-	//config.width = 1920;
-	//config.height = 1280;
-	
-	//config.width = 480;
-	//config.height = 320;
-	
-	//config.width = 1280;
-	//config.height = 720;
-	
-	//config.width = 800;
-	//config.height = 480;
-	
-	new LwjglApplication(new Dbash(), config);
+		//config.width = 1920;
+		//config.height = 1280;
+		
+		//config.width = 480;
+		//config.height = 320;
+		
+		//config.width = 1280;
+		//config.height = 720;
+		
+		//config.width = 800;
+		//config.height = 480;
+		
+		new LwjglApplication(new Dbash(), config);
 	}
 	
 	private static boolean isFullScreenArgument(String[] args) {
-	return args.length>1 && "-full-screen".equals(args[1]);
+		return args.length>1 && "-full-screen".equals(args[1]);
 	}
 	
 	public static void useMaximumDisplayMode(LwjglApplicationConfiguration cfg) {
