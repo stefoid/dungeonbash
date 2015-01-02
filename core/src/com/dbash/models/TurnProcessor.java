50364b391c2e71d70c100d9b9f2b3e6e47eaac50
Steven Mathers <stevenmathers@interfaceit.com>
2015-01-02 22:56:14 +1100

    thatts the lot

--- core/src/com/dbash/models/TurnProcessor.java
+++ core/src/com/dbash/models/TurnProcessor.java
@@ -456,9 +456,9 @@ public class TurnProcessor implements IPresenterTurnState {
 		if (L.TESTCHARS) {
 			theChars.clear();
 			p.level = 20;
-			theChars.add(new Character(Creature.getIdForName("manticore"), p, 1, dungeonEvents, dungeonQuery, this));
-			theChars.add(new Character(Creature.getIdForName("green dragon"), p, 2, dungeonEvents, dungeonQuery, this));
-			theChars.add(new Character(Creature.getIdForName("claw demon"), p, 3, dungeonEvents, dungeonQuery, this));
+			theChars.add(new Character(Creature.getIdForName("gorgon"), p, 1, dungeonEvents, dungeonQuery, this));
+			theChars.add(new Character(Creature.getIdForName("disrupter"), p, 2, dungeonEvents, dungeonQuery, this));
+			theChars.add(new Character(Creature.getIdForName("blodger"), p, 3, dungeonEvents, dungeonQuery, this));
 			Creature c = theChars.get(0);
 //			c.addAbility(new Ability(Ability.getIdForName("wand of percussion"), null, 20, dungeonEvents, dungeonQuery), null);
 //			c.addAbility(new Ability(Ability.getIdForName("wand: smashing"), null, 20, dungeonEvents, dungeonQuery), null);
