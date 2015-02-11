7f45b0051e8b5b1604f1151db8725d168126daf4
Steven Mathers <stevenmathers@interfaceit.com>
2015-02-11 21:26:10 +1100

    maybe clean

--- ios/src/com/me/dbash/IOSLauncher.java
+++ ios/src/com/me/dbash/IOSLauncher.java
@@ -3,7 +3,6 @@ package com.me.dbash;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIDevice;
-import org.robovm.bindings.crashlytics.Crashlytics;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
@@ -21,8 +20,6 @@ public class IOSLauncher extends IOSApplication.Delegate {
 
 	@Override
 	public void didFinishLaunching (UIApplication application) {
-		@SuppressWarnings("unused")
-		Crashlytics crashlytics = Crashlytics.start("2fae8bedad1e84766db4bddb94031c22de93804d");
 	}
     
     public static void main(String[] argv) {
