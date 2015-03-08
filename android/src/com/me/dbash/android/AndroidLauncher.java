3105a633719229b6aa26d7fc315b4f370692cded
Steven Mathers <stevenmathers@interfaceit.com>
2015-03-08 18:57:39 +1100

    RELEASE CANDIDATE A13I10

--- android/src/com/me/dbash/android/AndroidLauncher.java
+++ android/src/com/me/dbash/android/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.crashlytics.android.Crashlytics;
 //import com.crashlytics.android.Crashlytics;
 import com.me.dbash.Dbash;
 
@@ -11,7 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//Crashlytics.start(this);
+		Crashlytics.start(this);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Dbash(0), config);
 	}
