335f3d299c313f1ba8b874b8dec1f017959e326c
Steven Mathers <stevenmathers@interfaceit.com>
2015-04-14 20:25:14 +1000

    RELEASE A15I13

--- core/src/com/me/dbash/Dbash.java
+++ core/src/com/me/dbash/Dbash.java
@@ -34,7 +34,7 @@ import com.dbash.util.Rect;
 public class Dbash implements ApplicationListener {
 	public static final boolean LOG = false && L.DEBUG;
 	
-	public static String SAVE_FILE_VERISON = "V14";
+	public static String SAVE_FILE_VERISON = "V15";
 	
 	enum GameState {
 		SPLASH,
