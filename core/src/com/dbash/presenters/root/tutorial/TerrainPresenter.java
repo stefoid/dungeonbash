ba5db5e998fb5be25e6c3f891e7137d62845ac3a
Steven Mathers <stevenmathers@interfaceit.com>
2015-04-23 20:24:29 +1000

    added terrain and preparing for more stealth tutorial stuff

---
+++ core/src/com/dbash/presenters/root/tutorial/TerrainPresenter.java
@@ -0,0 +1,64 @@
+package com.dbash.presenters.root.tutorial;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.dbash.models.TouchEvent;
+import com.dbash.models.TouchEventListener;
+import com.dbash.models.TouchEventProvider;
+import com.dbash.platform.UIDepend;
+import com.dbash.presenters.root.IDismissListener;
+import com.dbash.presenters.root.OverlayPresenter;
+import com.dbash.util.Rect;
+import com.dbash.util.Rect.HAlignment;
+import com.dbash.util.Rect.VAlignment;
+
+
+public class TerrainPresenter extends OverlayPresenter implements TouchEventListener {
+	
+	FadeBoxPresenter fadeBox;
+	
+	public TerrainPresenter() {
+	}
+	
+	@Override
+	public void init(UIDepend gui) {
+		this.gui = gui;
+	}
+	
+	@Override
+	public void start(Rect theArea, TouchEventProvider touchEventProvider) {
+		this.touchEventProvider = touchEventProvider;
+		this.area = new Rect(gui.sizeCalculator.dungeonArea, .15f, .2f, .6f, .01f);
+		final Object me = this;
+		// Needs to swallow all touches to the dungeon area 
+		touchEventProvider.addTouchEventListener(this, gui.sizeCalculator.dungeonArea, gui.cameraViewPort.viewPort);  
+		addMoreFaderBoxes();
+	}
+	
+	private void addMoreFaderBoxes() {
+		FadeBoxPresenter fb1 = new FadeBoxPresenter("terrain1", 
+				HAlignment.CENTER, VAlignment.BOTTOM, null);
+		gui.overlayQueues.addSequential(fb1);
+		
+		final OverlayPresenter me = this;
+		FadeBoxPresenter fb2 = new FadeBoxPresenter("Terrain2", 
+				HAlignment.CENTER, VAlignment.BOTTOM, new IDismissListener() {
+			public void dismiss() {
+				me.dismiss();
+			}
+		});
+		gui.overlayQueues.addSequential(fb2);
+	}
+	@Override
+	public void draw(SpriteBatch spriteBatch, float x, float y) {
+	}
+	
+	@Override
+	public boolean touchEvent(TouchEvent event) {
+		return true;
+	}
+	
+	public void destroy() {
+		touchEventProvider.removeTouchEventListener(this);
+	}
+
+}
