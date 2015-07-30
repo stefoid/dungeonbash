9d0dfea73f6363e92e5ed692a7a6fa864d5ccf9a
Steven Mathers <stevenmathers@interfaceit.com>
2015-07-30 20:26:21 +1000

    removed supurflouous images

--- core/src/com/dbash/util/L.java
+++ core/src/com/dbash/util/L.java
@@ -4,7 +4,7 @@ public class L {
 
 	public static boolean DEBUG = false;
 	
-	public static boolean JARFILE = true;
+	public static boolean JARFILE = false;
 	
 	public static boolean TESTCHARS = false;
 	public static String c1 = "greater demon";
